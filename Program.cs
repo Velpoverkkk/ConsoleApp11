bfaf351ccc0c395f5c8652aea86142c3bfd5062e
Yura <Yura@DESKTOP-GAICMVJ>
2022-06-18 20:22:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{   //Задан массив из k сиволов.Определить количество различных элементов в массиве.
+    static void Main(string[] args)
+    {
+        String s;
+        int k, n = 0,l=0;
+        System.Console.WriteLine("Razmer massiva");
+        s = Console.ReadLine();
+        k = System.Convert.ToInt32(s);
+        int[] array = new int[k];
+        System.Console.WriteLine("Vvod massiva: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            s = Console.ReadLine();
+            array[i] = System.Convert.ToInt32(s);
+        }
+        for (int j = 0; j < array.Length; j++)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[j] == array[i])   n++;
+            }
+            if (n > 1) l++;
+            n = 0;
+        }
+        System.Console.WriteLine("kolvo raz el: " + (array.Length-l));
+
+    }
+}
